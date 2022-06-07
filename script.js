0b38c44e2fd81506d65400544958246dbf157051
MuhammaddiyorRR <muhammaddiyorrr@gmail.com>
2022-06-07 08:40:09 +0500

    m

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggless = document.querySelectorAll(".faq-toggle");
+
+toggless.forEach(toggle =>{
+  toggle.addEventListener('click', () =>{
+  toggle.parentNode.classList.toggle('active');
+
+  })
+})
